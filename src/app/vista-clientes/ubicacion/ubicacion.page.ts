850dcb1ce8a4eaa7f1e885dd18f60abe2b96084e
IsaacGE <jesusisaac360@gmail.com>
2022-04-13 11:53:56 -0500

    se agregó modulo nativo de geolocalizacion

--- src/app/vista-clientes/ubicacion/ubicacion.page.ts
+++ src/app/vista-clientes/ubicacion/ubicacion.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { LoadingController } from '@ionic/angular';
+
+declare var google;
 
 @Component({
   selector: 'app-ubicacion',
@@ -7,9 +11,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UbicacionPage implements OnInit {
 
-  constructor() { }
+  mapRef = null;
+  constructor(
+    private geolocation: Geolocation,
+    private loadingCtrl: LoadingController
+  ) {}
 
   ngOnInit() {
+    this.loadMap();
   }
 
+  async loadMap() {
+    const loading = await this.loadingCtrl.create();
+    loading.present();
+    const myLatLng = await this.getLocation();
+    const mapEle: HTMLElement = document.getElementById('map');
+    this.mapRef = new google.maps.Map(mapEle, {
+      center: myLatLng,
+      zoom: 8
+    });
+    google.maps.event
+    .addListenerOnce(this.mapRef, 'idle', () => {
+      loading.dismiss();
+      this.addMaker(myLatLng.lat, myLatLng.lng);
+    });
+  }
+
+  private addMaker(lat: number, lng: number) {
+    const marker = new google.maps.Marker({
+      position: { lat, lng },
+      map: this.mapRef,
+      title: 'Aqui estoy'
+    });
+  }
+
+  private async getLocation() {
+    const rta = await this.geolocation.getCurrentPosition();
+    return {
+      lat: rta.coords.latitude,
+      lng: rta.coords.longitude
+    };
+  }
 }
